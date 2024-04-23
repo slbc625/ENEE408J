f8a6762bc434444d590d427bea9405664276ec04
Jason Hsu <jasonahsu@yahoo.com>
2024-04-23 10:31:59 -0400

    added 75% overlap

--- autotuner_test/autotuner_test.ino
+++ autotuner_test/autotuner_test.ino
@@ -6,35 +6,70 @@
 #include <math.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S            i2s1;           //xy=318.16668701171875,157
-AudioEffectGranular      granular1;      //xy=546.1666870117188,149
-AudioEffectGranular      granular2;      //xy=572.1666717529297,206.1666717529297
-AudioAnalyzeNoteFrequency notefreq;       //xy=698.1666870117188,450
-AudioMixer4              mixer1;         //xy=746.1666717529297,146.1666717529297
-AudioOutputI2S           i2s2_1;         //xy=894.1666870117188,148
+AudioInputI2S            i2s1;           //xy=171.1666717529297,146
+AudioAnalyzeNoteFrequency notefreq;       //xy=267.16668701171875,550.9999694824219
+AudioEffectDelay         delay1;         //xy=359.16668701171875,212.16665649414062
+AudioEffectGranular      granular4;      //xy=506.16668701171875,574.1666870117188
+AudioEffectGranular      granular3;      //xy=542.1666870117188,430.16668701171875
+AudioEffectGranular      granular1;      //xy=544.1666870117188,155.00000762939453
+AudioEffectGranular      granular2;      //xy=578.1666870117188,298.1666564941406
+AudioEffectDelay         delay3;         //xy=750.1666870117188,283.16668701171875
+AudioEffectDelay         delay4;         //xy=750.1666870117188,428.16668701171875
+AudioEffectDelay         delay2;         //xy=752.1666870117188,143.16665649414062
+AudioEffectDelay         delay5;         //xy=752.1666870117188,578.1666870117188
+AudioMixer4              mixer1;         //xy=1034.1666870117188,163.16665649414062
+AudioOutputI2S           i2s2_1;         //xy=1169.1665649414062,168
 AudioConnection          patchCord1(i2s1, 0, notefreq, 0);
-AudioConnection          patchCord2(i2s1, 0, granular1, 0);
-AudioConnection          patchCord3(i2s1, 0, granular2, 0);
-AudioConnection          patchCord4(granular1, 0, mixer1, 0);
-AudioConnection          patchCord5(granular2, 0, mixer1, 1);
-AudioConnection          patchCord6(mixer1, 0, i2s2_1, 0);
+AudioConnection          patchCord2(i2s1, 0, delay1, 0);
+AudioConnection          patchCord3(delay1, 0, granular1, 0);
+AudioConnection          patchCord4(delay1, 1, granular2, 0);
+AudioConnection          patchCord5(delay1, 2, granular3, 0);
+AudioConnection          patchCord6(delay1, 3, granular4, 0);
+AudioConnection          patchCord7(granular4, delay5);
+AudioConnection          patchCord8(granular3, delay4);
+AudioConnection          patchCord9(granular1, delay2);
+AudioConnection          patchCord10(granular2, delay3);
+AudioConnection          patchCord11(delay3, 0, mixer1, 1);
+AudioConnection          patchCord12(delay4, 0, mixer1, 2);
+AudioConnection          patchCord13(delay2, 0, mixer1, 0);
+AudioConnection          patchCord14(delay5, 0, mixer1, 3);
+AudioConnection          patchCord15(mixer1, 0, i2s2_1, 0);
 // GUItool: end automatically generated code
 
-// GUItool: end automatically generated code
 #define GRANULAR_MEMORY_SIZE 12800  // enough for 290 ms at 44.1 kHz
 int16_t granularMemory1[GRANULAR_MEMORY_SIZE];
 int16_t granularMemory2[GRANULAR_MEMORY_SIZE];
+int16_t granularMemory3[GRANULAR_MEMORY_SIZE];
+int16_t granularMemory4[GRANULAR_MEMORY_SIZE];
 
 void setup() {
   Serial.begin(115200);
-  AudioMemory(30);
+  AudioMemory(256);
   notefreq.begin(.15);  //yin threshold
+
+  //set up the freqshifts
   granular1.begin(granularMemory1, GRANULAR_MEMORY_SIZE);
-  granular1.beginPitchShift(13);
+  granular1.beginPitchShift(12);
   granular2.begin(granularMemory2, GRANULAR_MEMORY_SIZE);
-  granular2.beginPitchShift(17);
-  mixer1.gain(0, 0.5);
-  mixer1.gain(1, 0.5);
+  granular2.beginPitchShift(12);
+  granular1.begin(granularMemory3, GRANULAR_MEMORY_SIZE);
+  granular1.beginPitchShift(12);
+  granular2.begin(granularMemory4, GRANULAR_MEMORY_SIZE);
+  granular2.beginPitchShift(12);
+  //merge together signals
+  mixer1.gain(0, 0.25);
+  mixer1.gain(1, 0.25);
+  mixer1.gain(2, 0.25);
+  mixer1.gain(3, 0.25);
+  //add delay
+  delay1.delay(0,0);
+  delay1.delay(1,3);
+  delay1.delay(2,6);
+  delay1.delay(3,9);
+  delay2.delay(0,9);
+  delay3.delay(0,6);
+  delay4.delay(0,3);
+  delay5.delay(0,9);
 }
 
 void loop() {
@@ -49,10 +84,12 @@ void loop() {
       
       granular1.setSpeed(ratio);
       granular2.setSpeed(ratio);
+      granular3.setSpeed(ratio);
+      granular4.setSpeed(ratio);
   }
 }
 float nearestFreq(float measured){
-  float rounded= log(measured/440.0)/log(2);
+  float rounded= log(measured/440.0)/log(2);//divide by 440, then take log2
   rounded=round(12*rounded)/12;//round to nearest 12th
   rounded=exp(log(2)*rounded)*440.0;
   return rounded;
